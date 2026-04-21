38886d49ac55d72768b48bbf4efaf1146407e556
deathwalker66 <storage.catnet@gmail.com>
2026-04-20 22:19:39 -0600

    [20/04/2026] | [22:19] - First Commit

---
+++ SCP/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+### Universal
+    # Paquetes temporales
+    rm -rf /var/cache/apk/* /var/cache/apt/* /var/lib/apt/lists/* /tmp/* /var/tmp/*
+    # Cache de paquetes
+    rm -rf ~/.cache /root/.cache
+
+### Specific
+    apk del openssl
---
+++ SCP/nonsimplificated.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/sbin/sshd -D &
+/usr/sbin/nginx -g "daemon off;" 
